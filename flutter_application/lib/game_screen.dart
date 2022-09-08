67f5b20bec3790017ce273e4177b827cac910434
Ketan Ramteke <Ketantoo@gmail.com>
2022-09-08 15:07:43 +0530

    Hangman game in flutter initial commit

---
+++ flutter_application/lib/game_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/utils.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text(
+          "Hangman",
+          style: restroStyle(20, Colors.white, FontWeight.bold),
+        ),
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.volume_up_sharp,
+                color: Colors.purpleAccent,
+              ))
+        ],
+      ),
+    );
+  }
+}
---
+++ flutter_application/lib/utils.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+TextStyle restroStyle(
+  double size,
+  Color color,
+  FontWeight fw,
+) {
+  return TextStyle(
+    fontFamily: "RetroGaming",
+    fontSize: size,
+    color: color,
+    fontWeight: fw,
+  );
+}
