735215f92fc7410e1bbfbc1922c67d61b6dc8228
Ketan Ramteke <Ketantoo@gmail.com>
2022-09-13 12:05:45 +0530

    added points, image, lifes section

--- flutter_application/lib/game_screen.dart
+++ flutter_application/lib/game_screen.dart
@@ -29,6 +29,40 @@ class _GameScreenState extends State<GameScreen> {
               ))
         ],
       ),
+      body: SingleChildScrollView(
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width / 3.5,
+                decoration: BoxDecoration(color: Colors.blue),
+                margin: EdgeInsets.only(top: 20),
+                child: Text(
+                  "12 Points",
+                  style: restroStyle(15, Colors.black, FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Image(
+                image: AssetImage("images/hangman0.png"),
+                width: 150,
+                height: 150,
+                color: Colors.white,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 20),
+              Text(
+                "5 lifes left",
+                style: restroStyle(15, Colors.grey, FontWeight.bold),
+              ),
+              
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
